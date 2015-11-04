7f928216485f74e97510735b599566a4d6c5024f
Koichi Murase <myoga.murase@gmail.com>
2015-11-05 03:10:18 +0900

    tools/gzjs/inflate/Makefile.pp: m, replace the output directory `out/' with `$(OUTDIR)/'.

--- tools/gzjs/inflate/Makefile.pp
+++ tools/gzjs/inflate/Makefile.pp
@@ -5,6 +5,11 @@ all:
 
 MWGPP:=mwg_pp.awk
 
+OUTDIR:=out
+
+$(OUTDIR):
+	mkdir -p $@
+
 Makefile: Makefile.pp
 	$(MWGPP) $< > $@
 
@@ -12,58 +17,58 @@ Makefile: Makefile.pp
 out/data.dat: ../../../out/agh.lang.tex.js
 	gzjs -Wno-gz -o$@ $<
 	sed -i -e '1s/^\xef\xbb\xbf//' -e '/[^]/!d' -e 's/$$//' $@
-out/data.dat.gz: out/data.dat
+$(OUTDIR)/data.dat.gz: out/data.dat | $(OUTDIR)
 	gzip -9 -c $< > $@
 gzcut.exe: gzcut.cpp
 	g++ -o $@ $<
-out/data-gz85.dat: out/data.dat.gz gzcut.exe
+$(OUTDIR)/data-gz85.dat: out/data.dat.gz gzcut.exe | $(OUTDIR)
 	./gzcut.exe $< > $@
-out/data-deflate+base64.dat: out/data.dat
+$(OUTDIR)/data-deflate+base64.dat: out/data.dat | $(OUTDIR)
 	gzjs -Wsfx -o - $< | sed -e '1,/replace(/d' -e "/^[^']/d" > $@
-out/data-deflate+base85.dat: out/data.dat
+$(OUTDIR)/data-deflate+base85.dat: out/data.dat | $(OUTDIR)
 	gzjs -Wsfx85 -o - $< | sed -e '1,/replace(/d' -e "/^[^']/d" > $@
 
 #%m frame64
 tests+=out/sfxtest-%suffix%.js
-out/sfxtest-%suffix%.js: frame-%suffix%.js sfxtest-b64.def.js out/data-deflate+base64.dat
+$(OUTDIR)/sfxtest-%suffix%.js: frame-%suffix%.js sfxtest-b64.def.js out/data-deflate+base64.dat | $(OUTDIR)
 	cat sfxtest-b64.def.js $< | $(MWGPP) > $@
 frames+=out/sfxframe-%suffix%.js
-out/sfxframe-%suffix%.js: frame-%suffix%.js sfxframe-b64.def.js
+$(OUTDIR)/sfxframe-%suffix%.js: frame-%suffix%.js sfxframe-b64.def.js | $(OUTDIR)
 	cat sfxframe-b64.def.js $< | $(MWGPP) > $@
 #%end
 #%x frame64.r|%suffix%|20090720|
 #%x frame64.r|%suffix%|20090728|
 
 tests+=out/sfxtest-a85-v1.js
-out/sfxtest-a85-v1.js: frame-a85-v1.js impl_inflate.js impl_a85.js impl_utf8.js sfxtest-a85.def.js out/data-gz85.dat
+$(OUTDIR)/sfxtest-a85-v1.js: frame-a85-v1.js impl_inflate.js impl_a85.js impl_utf8.js sfxtest-a85.def.js out/data-gz85.dat | $(OUTDIR)
 	cat sfxtest-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 frames+=out/sfxframe-a85-v1.js
-out/sfxframe-a85-v1.js: frame-a85-v1.js impl_inflate.js impl_a85.js impl_utf8.js sfxframe-a85.def.js
+$(OUTDIR)/sfxframe-a85-v1.js: frame-a85-v1.js impl_inflate.js impl_a85.js impl_utf8.js sfxframe-a85.def.js | $(OUTDIR)
 	cat sfxframe-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 
-out/.frame-a85-v2gen1.js: frame-a85-v2.js impl_inflate.js impl_a85.js impl_utf8.js
+$(OUTDIR)/.frame-a85-v2gen1.js: frame-a85-v2.js impl_inflate.js impl_a85.js impl_utf8.js | $(OUTDIR)
 	PPC_CPP=1 $(MWGPP) $< > $@
-out/.frame-a85-v2gen2.js: out/.frame-a85-v2gen1.js
+$(OUTDIR)/.frame-a85-v2gen2.js: out/.frame-a85-v2gen1.js | $(OUTDIR)
 	gzjs -Wno-gz -Wrtok -o$@ $<
 tests+=out/sfxtest-a85-v2.js
-out/sfxtest-a85-v2.js: frame-a85-v2f.js out/.frame-a85-v2gen2.js sfxtest-a85.def.js out/data-gz85.dat
+$(OUTDIR)/sfxtest-a85-v2.js: frame-a85-v2f.js out/.frame-a85-v2gen2.js sfxtest-a85.def.js out/data-gz85.dat | $(OUTDIR)
 	cat sfxtest-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 frames+=out/sfxframe-a85-v2.js
-out/sfxframe-a85-v2.js: frame-a85-v2f.js out/.frame-a85-v2gen2.js sfxframe-a85.def.js
+$(OUTDIR)/sfxframe-a85-v2.js: frame-a85-v2f.js out/.frame-a85-v2gen2.js sfxframe-a85.def.js | $(OUTDIR)
 	cat sfxframe-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 
-out/.frame-a85-v3gen1.js: out/.frame-a85-v2gen1.js
+$(OUTDIR)/.frame-a85-v3gen1.js: out/.frame-a85-v2gen1.js | $(OUTDIR)
 	uglifyjs -c -m -o $@ $<
 	sed -i -e 's/^!function/(function/' -e 's/();\{0,1\}$$/)()/' -e 's/\bassign\b/A/g' -e 's/\bstatus\b/B/g' -e 's/\broot\b/C/g' -e 's/\blist\b/D/g' $@
-out/.frame-a85-v3gen2.js: out/.frame-a85-v3gen1.js
+$(OUTDIR)/.frame-a85-v3gen2.js: out/.frame-a85-v3gen1.js | $(OUTDIR)
 	gzjs -Wno-gz -Wrtok -Wno-fold-string -o$@ $<
 tests+=out/sfxtest-a85-v3.js
-out/.frame-a85-v3f.js: frame-a85-v2f.js
+$(OUTDIR)/.frame-a85-v3f.js: frame-a85-v2f.js | $(OUTDIR)
 	sed 's/v2/v3/g' $< > $@
-out/sfxtest-a85-v3.js: out/.frame-a85-v3f.js out/.frame-a85-v3gen2.js sfxtest-a85.def.js out/data-gz85.dat
+$(OUTDIR)/sfxtest-a85-v3.js: out/.frame-a85-v3f.js out/.frame-a85-v3gen2.js sfxtest-a85.def.js out/data-gz85.dat | $(OUTDIR)
 	cat sfxtest-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 frames+=out/sfxframe-a85-v3.js
-out/sfxframe-a85-v3.js: out/.frame-a85-v3f.js out/.frame-a85-v3gen2.js sfxframe-a85.def.js
+$(OUTDIR)/sfxframe-a85-v3.js: out/.frame-a85-v3f.js out/.frame-a85-v3gen2.js sfxframe-a85.def.js | $(OUTDIR)
 	cat sfxframe-a85.def.js $< | PPC_CPP=1 $(MWGPP) > $@
 
 tests: $(tests)
